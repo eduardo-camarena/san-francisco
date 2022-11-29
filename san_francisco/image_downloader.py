c162570495fcb2427bc45061b8c69205fa863282
Eduardo Camarena <eduardo@creditus.mx>
2022-11-29 16:33:34 -0600

    added progress bar to project

--- san_francisco/image_downloader.py
+++ san_francisco/image_downloader.py
@@ -1,35 +1,40 @@
 import requests
 import shutil
 import typer
+from progress.bar import Bar
 
 app = typer.Typer()
 
 @app.command()
 def download_images(
   base_url: str,
+  save_to: str,
   last: int,
-  to: str,
   first: int = 0,
   extension: str = '.jpg',
   add_extension: bool = False,
   zeroes: int = 0,
-  query: str = ''
+  query: str = '',
+  name: str = ''
 ):
   query = '' if query == '' else f'?{query}'
-  for i in range(first, last):
-    filler = '0' * (zeroes + 1 - len(f'{i + 1}'))
-    picture_number = f'{filler}{i + 1}'
+  with Bar('Downloading', max=last - first) as bar:
+    for i in range(first, last):
+      filler = '0' * (zeroes + 1 - len(f'{i + 1}'))
+      picture_number = f'{filler}{i + 1}'
 
-    request_extension = extension if add_extension else '';
-    request_url = f'{base_url}{picture_number}{request_extension}{query}'
-  
-    print(request_url)
-    r = requests.get(request_url, stream=True)
+      request_extension = extension if add_extension else '';
+      request_url = f'{base_url}{picture_number}{request_extension}{query}'
 
-    if r.status_code == 200:
-      with open(f'{to}/{picture_number}{extension}', 'wb') as f:
-        r.raw.decode_content = True
-        shutil.copyfileobj(r.raw, f)
+      r = requests.get(request_url, stream=True)
+
+      picture_name = f'{name} ({i + 1})' if name else f'{i + 1}'
+      if r.status_code == 200:
+        with open(f'{save_to}/{picture_name}{extension}', 'wb') as f:
+          r.raw.decode_content = True
+          shutil.copyfileobj(r.raw, f)
+
+      bar.next()
 
 
 def main():
